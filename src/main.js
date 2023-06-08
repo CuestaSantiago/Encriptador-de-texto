70c2ed667e25d1a9bd1be9487839643974cc7eeb
Santiago <gosanti988@gmail.com>
2023-06-07 20:59:24 -0500

    se agrego validacion para acentos y estilos

--- src/main.js
+++ src/main.js
@@ -6,14 +6,24 @@ let mensajeEncriptado ="";
 const totalVocales= vocales.length
 let index = 0
 let mensaje = document.getElementById("recibirMensaje");
-const init= ()=>{  
+let mensajeError = document.getElementById("mensaje_vacio")
+let btnCopiar = document.getElementById("btn_copiar")
+let btnLimpiar = document.getElementById("btn_limpiar")
 
-  // if (mensaje.length >0) {
+
+const init= ()=>{  
+  const regex = /[áéíóúÁÉÍÓÚ]/g;
+  const tildesEncontradas = mensaje.value.match(regex);
+  if (tildesEncontradas === null) {
+  if (mensaje.value.length >0 ) {
+    btnLimpiar.style.display ="block"
     return mensaje.value.trim().split(separador);
-  // }else{
-  //   mostrarMensajeError()
-  // }
-   
+  }else{
+    mensajeError.style.display ="block"
+  }
+} else{
+  alert("No se permiten acentos")
+}
 }
 
 const encriptarMensaje = () => {
@@ -24,6 +34,8 @@ let msmRecibir = document.getElementById("darMensaje")
   };
   msmRecibir.value = mensajeEncriptado.trim();
   msmRecibir.style.backgroundImage = "none"
+  mensajeError.style.display ="none"
+  btnCopiar.style.display = "block"
   mensajeEncriptado ="";
 };
 
@@ -60,4 +72,32 @@ function desencriptar(mensaje) {
     mensajeRescrito = mensajeRescrito.replaceAll(vocalesEncriptadas[contador], vocales[contador])
   }
   return mensajeRescrito;
+}
+
+function copiarMensaje() {
+  var textarea = document.getElementById("darMensaje").value;
+  navigator.clipboard.writeText(textarea)
+  .then(() => {
+    alert("¡El mensaje se ha copiado al portapapeles!");
+  })
+  .catch(err => {
+      console.error('Error in copying text: ', err);
+  });
+  
+}
+
+function clearTextarea() {
+  mensaje.value = "";
+}
+
+function showMessage() {
+  var message = document.querySelector('.message');
+  message.style.visibility = 'visible';
+  message.style.opacity = 1;
+}
+
+function hideMessage() {
+  var message = document.querySelector('.message');
+  message.style.visibility = 'hidden';
+  message.style.opacity = 0;
 }
